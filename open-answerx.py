ca80139d6e0d13559f4a10cd5e6f587054179364
Diego Xirinachs <dxirinac@akamai.com>
2019-03-22 13:04:13 -0600

    added CIDR and expiry functions, modified insert domain function, bug fixes and improvements

--- open-answerx.py
+++ open-answerx.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import urllib
 import xml.dom.minidom
 import sys
 import os
@@ -9,12 +11,14 @@ from urlparse import urljoin
 edgerc = EdgeRc('.edgerc')
 
 parser = argparse.ArgumentParser(description='Interact with the AnswerX OPEN API to view or insert table data')
-parser.add_argument('-t', '--table', help='Table to target, displays the table schema', required=True)
+parser.add_argument('-t', '--table', help='Table to target, displays the table schema if dump is not provided', required=True)
 parser.add_argument('-d', '--dump', help='Dump/Display the contents of the table', action='store_true')
 parser.add_argument('-r', '--remove', help='Remove a domain from a table', type=str)
 parser.add_argument('-D', '--debug', help='Enable Pragma headers for troubleshooting', action='store_true')
 parser.add_argument('-i', '--insert', help='Toggle insert mode, if this is set, you need to specify what do you want to insert into the table', type=str)
+parser.add_argument('-x', '--expiry', help='If insert mode is true, set the expiry time for the realtime table entry (TTL), default is 60 seconds', type=int)
 parser.add_argument('-S', '--subscriber', help='Check subscriberID belongs to a table', type=str)
+parser.add_argument('-C', '--cidr', help='Check CIDR belongs to a table', type=str)
 parser.add_argument('-s', '--static', help='Tells the API to query a static table and not a real-time table', action='store_true')
 parser.add_argument('-e', '--environment', help='Set target environment, this is a number related to the .edgerc section that contains the credentials where you want to run the script, if not specified, then the default section is used', type=int)
 args = vars(parser.parse_args())
@@ -48,7 +52,7 @@ def showTable(tablename):
         table_url = urljoin(baseurl, '/recursive-dns-db/v1/service-instances/' + str(
             service_instance_id) + '/tables/' + tablename + '?action=dump&' + table_type)
     else:
-        table_url = urljoin(baseurl, '/recursive-dns-db/v1/service-instances/'+str(service_instance_id)+'/tables/'+tablename+'?'+table_type)
+        table_url = urljoin(baseurl, '/recursive-dns-db/v1/service-instances/'+str(service_instance_id)+'/tables/'+tablename+'?'+'action=schema'+'&'+table_type)
 
     table_data = s.get(table_url, stream=True)
     print table_data.headers
@@ -73,24 +77,51 @@ def showSub(tablename):
     print subscriber.content
     return subscriber
 
+def encodeCIDR(cidr):
+
+    noslash_cidr = urllib.quote(cidr, safe='')
+    encoded_cidr = str(noslash_cidr).replace(".","%2E")
+
+    return encoded_cidr
+
+def showCIDR(tablename):
+    key = args['cidr']
+    key = encodeCIDR(key)
+
+    table_url = urljoin(baseurl,
+                        '/recursive-dns-db/v1/service-instances/' + str(service_instance_id) + '/tables/' + tablename)
+    cidr_url = urljoin(table_url, '?key=' + key)
+
+    cidr = s.get(cidr_url)
+    print 'GOT HTTP CODE: ' + str(cidr.status_code) + '\n' + cidr_url + '\n'
+
+    try:
+        global xml
+        xml = xml.dom.minidom.parseString(cidr.content)
+        return xml.toprettyxml()
+    except xml.parsers.expat.ExpatError as e:
+        return cidr
+
 def insertDomain(tablename):
 
     s.headers = {'Accept': 'application/json'}
 
-    data = """
-    {
-  "Expiry": 60,
-  "Columns": []
-}
-    """
-
     key = args['insert']
+
+    if args['expiry']:
+        expiry = args['expiry']
+    else:
+        expiry = 60
+
+    data = {"Expiry": expiry, }
+    data["Field"] = [{"Name": key, "Type": "STRING"}]
+    jsondata = json.dumps(data, sort_keys=True, indent=2)
 
     table_url = urljoin(baseurl,'/recursive-dns-db/v1/service-instances/' + str(service_instance_id) + '/tables/' + tablename)
     post_url = urljoin(table_url,'?key=%22'+ key +'%22')
 
-
-    table_post = s.post(post_url,data=data)
+    print jsondata
+    table_post = s.post(post_url,data=jsondata)
     print 'GOT HTTP CODE: ' + str(table_post.status_code) + '\n' + post_url + '\n'
     print table_post.content
     return table_post
@@ -117,5 +148,7 @@ if __name__ == '__main__':
         print removeDomain(args['table'])
     if args['subscriber']:
         print showSub(args['table'])
+    if args['cidr']:
+        print showCIDR(args['table'])
     else:
         print showTable(args['table'])
