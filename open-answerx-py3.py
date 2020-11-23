5d332f03405edd52c1aeda87699e327d36c6a5e4
Diego Xirinachs <dxirinac@akamai.com>
2020-11-23 15:40:16 -0600

    New insertjson method for inserting custom json formatted data

--- open-answerx-py3.py
+++ open-answerx-py3.py
@@ -3,6 +3,7 @@
 import argparse
 import json
 import os
+import sys
 import urllib.error
 import urllib.parse
 import urllib.request
@@ -24,6 +25,8 @@ parser.add_argument('-d', '--dump', help='Dump/Display the contents of the table
 parser.add_argument('-r', '--remove', help='Remove a domain from a table', type=str)
 parser.add_argument('-D', '--debug', help='Enable Pragma headers for troubleshooting', action='store_true')
 parser.add_argument('-i', '--insert', help='Toggle insert mode, if this is set, you need to specify what do you want to insert into the table', type=str)
+parser.add_argument('-j', '--json', help='Insert custom JSON data from a file. You need to specify what do you want to insert into the table', type=str)
+parser.add_argument('-k', '--key', help='If using custom JSON data from a file, you must specify a key, this is usually the domain name or subscriber', type=str)
 parser.add_argument('-x', '--expiry', help='If insert mode is true, set the expiry time for the realtime table entry (TTL), default is 60 seconds', type=int)
 parser.add_argument('-S', '--subscriber', help='Check subscriberID belongs to a table', type=str)
 parser.add_argument('-C', '--cidr', help='Check CIDR belongs to a table', type=str)
@@ -57,17 +60,22 @@ def showTable(tablename):
     else:
         table_type = 'table_type=rtt'
 
-    if args['dump'] == True:
+    if args['dump']:
+        data_format = "txt"
         table_url = urljoin(baseurl, '/recursive-dns-db/v1/service-instances/' + str(
             service_instance_id) + '/tables/' + tablename + '?action=dump&' + table_type)
     else:
-        table_url = urljoin(baseurl, '/recursive-dns-db/v1/service-instances/'+str(service_instance_id)+'/tables/'+tablename+'?'+'action=schema'+'&'+table_type)
+        data_format = "json"
+        table_url = urljoin(baseurl, '/recursive-dns-db/v1/service-instances/'+str(service_instance_id)+'/tables/' + tablename + '?'+'action=schema'+'&'+table_type)
 
     table_data = s.get(table_url, stream=True)
     print(table_data.headers)
     print('GOT HTTP CODE: ' + str(table_data.status_code) + '\n' + table_url + '\n')
-    return table_data.text
-
+    if data_format == "json":
+        table_rows = json.dumps(table_data.json(), indent=2)
+    else:
+        table_rows = table_data.text
+    return table_rows
 
 def showSub(tablename):
     key = args['subscriber']
@@ -84,7 +92,7 @@ def showSub(tablename):
 def encodeCIDR(cidr):
 
     noslash_cidr = urllib.parse.quote(cidr, safe='')
-    encoded_cidr = str(noslash_cidr).replace(".","%2E")
+    encoded_cidr = str(noslash_cidr).replace(".", "%2E")
 
     return encoded_cidr
 
@@ -114,15 +122,14 @@ def insertDomain(tablename):
     else:
         expiry = 315360000
 
-    data = {"Expiry": expiry, }
-    data["Field"] = [{"Name": key, "Type": "STRING"}]
+    data = {"Expiry": expiry, "Field": [{"Name": key, "Type": "STRING"}]}
     jsondata = json.dumps(data, sort_keys=True, indent=2)
 
-    table_url = urljoin(baseurl,'/recursive-dns-db/v1/service-instances/' + str(service_instance_id) + '/tables/' + tablename)
-    post_url = urljoin(table_url,'?key=%22'+ key +'%22')
+    table_url = urljoin(baseurl, '/recursive-dns-db/v1/service-instances/' + str(service_instance_id) + '/tables/' + tablename)
+    post_url = urljoin(table_url, '?key=%22' + key + '%22')
 
     print(jsondata)
-    table_post = s.post(post_url,data=jsondata)
+    table_post = s.post(post_url, data=jsondata)
     print('GOT HTTP CODE: ' + str(table_post.status_code) + '\n' + post_url + '\n')
     #print(table_post.text)
     return table_post.text
@@ -132,8 +139,8 @@ def removeDomain(tablename):
 
     key = args['remove']
 
-    table_url = urljoin(baseurl,'/recursive-dns-db/v1/service-instances/' + str(service_instance_id) + '/tables/' + tablename)
-    delete_url = urljoin(table_url,'?key=%22'+ key +'%22')
+    table_url = urljoin(baseurl, '/recursive-dns-db/v1/service-instances/' + str(service_instance_id) + '/tables/' + tablename)
+    delete_url = urljoin(table_url, '?key=%22' + key + '%22')
 
     domain_delete = s.delete(delete_url)
     print('GOT HTTP CODE: ' + str(domain_delete.status_code) + '\n' + delete_url + '\n')
@@ -141,12 +148,37 @@ def removeDomain(tablename):
     return domain_delete.text
 
 
+def insertjson(json_file, tablename, key):
+
+    key = args['key']
+    s.headers = {'Accept': 'application/json'}
+    # Give an arbitrary piece of json data on a file and insert it on a table, useful for custom schemas not covered
+    # on the other methods
+    # key = args['json']
+    table_url = urljoin(baseurl, '/recursive-dns-db/v1/service-instances/' + str(service_instance_id) + '/tables/' + tablename)
+    with open(json_file) as file:
+        json_data = json.load(file)
+    print(json.dumps(json_data, indent=2))
+    # Get the key from the arguments, which is the first field
+    # key = (json_data["Columns"][0]["Value"])
+    key_url = urljoin(table_url, '?key=%22' + key + '%22')
+    json_data_post = s.post(key_url, json=json_data)
+    print('GOT HTTP CODE: ' + str(json_data_post.status_code) + '\n' + key_url + '\n')
+    return json_data_post.content
+
+
 if __name__ == '__main__':
     if args['insert']:
-        print("Inserting domain" + args['insert'] + "into " + args['table'])
+        print("Inserting domain " + args['insert'] + " into " + args['table'])
         print(insertDomain(args['table']))
+    if args['json']:
+        if not args['key']:
+            print("You must provide a key value to insert JSON data, this is usually the domain or subscriber ID")
+            sys.exit()
+        print("JSON Data file provided, inserting into table " + args['table'])
+        print(insertjson(args['json'], args['table'], args['key']))
     if args['remove']:
-        print("Removing domain " + args['remove'] + "from " + args['table'])
+        print("Removing domain " + args['remove'] + " from " + args['table'])
         print(removeDomain(args['table']))
     if args['subscriber']:
         print(showSub(args['table']))
